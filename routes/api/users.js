5105fb991138818780a6c8de26e71d745304824b
jennormous <smj.boudreau@gmail.com>
2020-11-28 21:28:31 -0800

    Added jwt authenication for users. Tested in Postman.

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
-const { check, validationResult } = require("express-validator/check");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+const { check, validationResult } = require("express-validator");
 
 const Users = require("../../models/Users");
 
@@ -57,13 +60,27 @@ router.post(
 
 			await user.save();
 
+			const payload = {
+				user: {
+					id: user.id
+				}
+			};
+
+			jwt.sign(
+				payload,
+				config.get("jwtSecret"),
+				{ expiresIn: 360000 },
+				(err, token) => {
+					if (err) throw err;
+					res.json({ token });
+				}
+			);
+
 			res.send("User Registered");
 		} catch (err) {
 			console.error(err.message);
 			res.status(500).send("Server error");
 		}
-
-		// Encrypt password
 
 		// Return JSON webtoken
 	}
